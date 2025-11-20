2b581141eabfb117b2ab1483f58e775cb5eb8fa5
shreevardhini2812 <shreevardhini28122000@gmail.com>
2025-11-20 21:55:47 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://resplendent-arithmetic-69478f.netlify.app/login",
+    credentials: true
+}));
 
 
-const PORT = process.env.PORT || "https://resplendent-arithmetic-69478f.netlify.app/login"
+const PORT = process.env.PORT || 5000
 connectDB(process.env.MONGO_URI);
 
 
